d36fb05e4d24ecd9614c7ca3997523e3a6d86980
ARUNG2905 <arunsiddu0315@gmail.com>
2025-06-13 23:11:24 +0530

    commit of interviewSession

--- prepinterview/src/main/java/com/project/prepinterview/controller/InterviewSessionController.java
+++ prepinterview/src/main/java/com/project/prepinterview/controller/InterviewSessionController.java
@@ -3,6 +3,6 @@ package com.project.prepinterview.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TestController {
+public class InterviewSessionController {
 
 }
